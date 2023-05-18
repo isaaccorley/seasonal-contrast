0085e886f5e28ec56eef6dd7db4c66e7771c80bf
isaaccorley <22203655+isaaccorley@users.noreply.github.com>
2023-05-18 22:21:07 +0000

    add more hyperparams

--- datasets/eurosat_datamodule.py
+++ datasets/eurosat_datamodule.py
@@ -7,9 +7,11 @@ from datasets.eurosat_dataset import EurosatDataset
 
 class EurosatDataModule(LightningDataModule):
 
-    def __init__(self, data_dir, transforms=T.ToTensor()):
+    def __init__(self, data_dir, batch_size=32, num_workers=8, transforms=T.ToTensor()):
         super().__init__()
         self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
         self.transforms = transforms
 
     @property
@@ -24,9 +26,9 @@ class EurosatDataModule(LightningDataModule):
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
-            batch_size=32,
+            batch_size=self.batch_size,
             shuffle=True,
-            num_workers=8,
+            num_workers=self.num_workers,
             drop_last=True,
             pin_memory=True
         )
@@ -38,7 +40,7 @@ class EurosatDataModule(LightningDataModule):
             shuffle=False,
             num_workers=8,
             drop_last=False,
-            pin_memory=True
+            pin_memory=False
         )
 
     def test_dataloader(self):
@@ -48,5 +50,5 @@ class EurosatDataModule(LightningDataModule):
             shuffle=False,
             num_workers=8,
             drop_last=False,
-            pin_memory=True
+            pin_memory=False
         )
--- main_eurosat.py
+++ main_eurosat.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from copy import deepcopy
 from argparse import ArgumentParser
 
+import torch.nn.functional as F
 import torchvision.transforms as T
 import torch
 from torch import nn, optim
@@ -17,9 +18,12 @@ from models.moco2_module import MocoV2
 
 class Classifier(LightningModule):
 
-    def __init__(self, backbone, in_features, num_classes):
+    def __init__(self, backbone, in_features, num_classes, weight_decay, norm):
         super().__init__()
         self.encoder = backbone
+        self.weight_decay = weight_decay
+        self.norm = norm
+
         for param in self.encoder.parameters():
             param.requires_grad = False
         self.classifier = nn.Linear(in_features, num_classes)
@@ -29,6 +33,8 @@ class Classifier(LightningModule):
     def forward(self, x):
         with torch.no_grad():
             feats = self.encoder(x)
+            if self.norm:
+                feats = F.normalize(feats, p=2, dim=-1)
         logits = self.classifier(feats)
         return logits
 
@@ -58,7 +64,7 @@ class Classifier(LightningModule):
         return loss, acc
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.classifier.parameters())
+        optimizer = optim.Adam(self.classifier.parameters(), weight_decay=self.weight_decay)
         max_epochs = self.trainer.max_epochs
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(0.6*max_epochs), int(0.8*max_epochs)])
         return [optimizer], [scheduler]
@@ -69,36 +75,56 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument('--gpus', type=int, default=1)
-    parser.add_argument('--data_dir', type=str)
-    parser.add_argument('--backbone_type', type=str, default='imagenet')
-    parser.add_argument('--ckpt_path', type=str, default=None)
+    parser.add_argument('--data-dir', type=str)
+    parser.add_argument("--num-workers", type=int, default=32)
+    parser.add_argument("--batch-size", type=int, default=32)
+    parser.add_argument("--backbone", default="resnet18", choices=["resnet18", "resnet50"])
+    parser.add_argument('--weights', type=str, default="imagenet", choices=["random", "imagenet", "pretrain"])
+    parser.add_argument('--ckpt-path', type=str, default=None)
+    parser.add_argument("--weight-decay", type=float, default=0.0)
+    parser.add_argument("--epochs", type=int, default=50)
+    parser.add_argument("--norm", action="store_true")
     args = parser.parse_args()
 
     transforms = [T.ToTensor(), T.Resize((224, 224))]
-    if args.backbone_type == "imagenet":
+    if args.weights == "imagenet":
         IMAGENET_MEAN = torch.tensor([0.485, 0.456, 0.406])
         IMAGENET_STD = torch.tensor([0.229, 0.224, 0.225])
         transforms.append(T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD))
 
-    datamodule = EurosatDataModule(args.data_dir, transforms=T.Compose(transforms))
+    datamodule = EurosatDataModule(args.data_dir, batch_size=args.batch_size, num_workers=args.num_workers, transforms=T.Compose(transforms))
 
-    if args.backbone_type == 'random':
-        backbone = resnet.resnet18(pretrained=False)
-        backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
-    elif args.backbone_type == 'imagenet':
-        backbone = resnet.resnet18(pretrained=True)
-        backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
-    elif args.backbone_type == 'pretrain':
+    if args.weights == "random":
+        if args.backbone == "resnet18":
+            backbone = resnet.resnet18(pretrained=False)
+            backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
+        else:
+            backbone = resnet.resnet50(pretrained=False)
+            backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
+    elif args.weights == "imagenet":
+        if args.backbone == "resnet18":
+            backbone = resnet.resnet18(pretrained=True)
+            backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
+        else:
+            backbone = resnet.resnet50(pretrained=True)
+            backbone = nn.Sequential(*list(backbone.children())[:-1], nn.Flatten())
+    elif args.weights == "pretrain":
         model = MocoV2.load_from_checkpoint(args.ckpt_path)
         backbone = deepcopy(model.encoder_q)
     else:
         raise ValueError()
 
-    model = Classifier(backbone, in_features=512, num_classes=datamodule.num_classes)
-    model.example_input_array = torch.zeros((1, 3, 64, 64))
+    model = Classifier(
+        backbone,
+        in_features=512 if args.backbone == "resnet18" else 2048,
+        num_classes=datamodule.num_classes,
+        weight_decay=args.weight_decay,
+        norm=args.norm
+    )
+    model.example_input_array = torch.zeros((1, 3, 224, 224))
 
-    experiment_name = args.backbone_type
+    experiment_name = f"{args.weights}_{args.backbone}_{args.weight_decay}"
     logger = TensorBoardLogger(save_dir=str(Path.cwd() / 'logs' / 'eurosat'), name=experiment_name)
-    trainer = Trainer(gpus=args.gpus, logger=logger, checkpoint_callback=True, max_epochs=100, weights_summary='full')
+    trainer = Trainer(gpus=[args.gpus], logger=logger, checkpoint_callback=True, max_epochs=args.epochs)
     trainer.fit(model, datamodule=datamodule)
     trainer.test(datamodule=datamodule, ckpt_path="best")
